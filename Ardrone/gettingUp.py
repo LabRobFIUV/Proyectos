1b2275ba92af716d26a9916ae3b9feca89b952df
Ellebkey <ellebkey@gmail.com>
2015-09-07 17:02:48 -0500

    Se agregaron los controles y las condiciones

--- Ardrone/gettingUp.py
+++ Ardrone/gettingUp.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import cv
 import libardrone
 import time as t
 
@@ -9,11 +10,12 @@ def main():
 	drone.reset()
 	drone.set_camera_view(1)
 	print "Presiona W para subir"
-	
+	drone.speed = 0.1
+	i = 1
 
 	while True:
 		running = True
-		
+
 		try:
 			start = t.time()
 			frame = drone.get_image()
@@ -25,15 +27,64 @@ def main():
 			if (tecla > 0):
 				print tecla
 
-			#Tecla W
-			if (tecla == 1048695):
-				drone.speed = 0.1
+			#Tecla P / Take photo
+			if (tecla == 1048603) and (running == True):
+				print "P"
+				cv.SaveImage("photo-" + str(i) + ".jpeg",frame)
+				i+=1
+				print i
+				running == False
+				break 
+			#Tecla U / Subir
+			elif (tecla == 1048693):
+				print "Subiendo"
 				while (t.time() < start + segundos) and (running == True):
 					print "subiendo"
 				running = False
 				print "acabo"
+			#Tecla W / Move Forward
+			elif (tecla == 1048695):
+				print "W"
+				#drone.move_forward()
+			#Tecla A / Move Left
+			elif (tecla == 1048673):
+				print "A"
+				#drone.move_left()
+			#Tecla S / Move Back
+			elif (tecla == 1048691):
+				print "S"
+				#drone.move_backward()
+			#Tecla D / Move Right
+			elif (tecla == 1048676):
+				print "D"
+				#drone.move_right()
+
+			#Tecla Up / Up
+			elif (tecla == 1113938):
+				print "Arriba"
+				#drone.move_up()
+			#Tecla Down / Down
+			elif (tecla == 1113940):
+				print "Abajo"
+				#drone.move_down()
+			#Tecla Right / Turn Right
+			elif (tecla == 1113939):
+				print "Derecha"
+				#drone.turn_right()
+			#Tecla Left / Turn Left
+			elif (tecla == 1113937):
+				print "Izquierda"
+				#drone.turn_left()
+			#Tecla Space
+			elif (tecla == 1048608):
+				print "Espacio"
+				#drone.land()
+			#Tecla Enter
+			elif (tecla == 1048586):
+				print "Enter"
+				#drone.takeoff()
 			#Escape
-			if (tecla == 1048603):
+			elif (tecla == 1048603):
 				cv2.destroyAllWindows()
 				drone.reset()
 				drone.halt()

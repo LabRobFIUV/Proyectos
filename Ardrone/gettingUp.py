1d842525e32848ca89669e172571ef4b31609232
Ellebkey <ellebkey@gmail.com>
2015-09-15 05:57:29 -0500

    Agregado estabilizador con tecla

--- Ardrone/gettingUp.py
+++ Ardrone/gettingUp.py
@@ -5,6 +5,7 @@ import libardrone
 import time as t
 
 def main():
+	font = cv2.FONT_HERSHEY_SIMPLEX
 	segundos = 5
 	drone = libardrone.ARDrone(True)
 	drone.reset()
@@ -14,29 +15,33 @@ def main():
 	i = 1
 
 	while True:
-		running = True
 
 		try:
 			start = t.time()
 			frame = drone.get_image()
 			frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-			cv2.imshow('frame',frame)
+			
 			bat = drone.navdata.get(0, dict()).get('battery', 0)
 			alt = drone.navdata.get(0, dict()).get('altitude', 0)
-			print "Bateria:" + str(bat)
-			print "\nAltura" + str(alt)
+			
+			cv2.putText(frame,"Bateria: "+str(bat),(20,20), font, 0.5,(0,0,255),2)
+			cv2.putText(frame,"Altura: "+str(alt),(500,20), font, 0.5,(255,0,0),2)
+			cv2.imshow('frame',frame)
+
 			tecla = cv2.waitKey(15)
 			
 			if (tecla > 0):
 				print tecla
 
 			#Tecla P / Take photo
-			if (tecla == 1048603) and (running == True):
+			if (tecla == 1048688):
 				print "P"
 				cv2.imwrite("photo-" + str(i) + ".jpeg",frame)
 				i+=1
-				print i
-				running = False
+			#Estabilizar
+			elif (tecla == 1048684):
+				print "estabilizado"
+				drone.hover()
 			#Tecla U / Subir
 			elif (tecla == 1048693):
 				print "Subiendo"
@@ -47,48 +52,48 @@ def main():
 			#Tecla W / Move Forward
 			elif (tecla == 1048695):
 				print "W"
-				#drone.move_forward()
+				drone.move_forward()
 			#Tecla A / Move Left
 			elif (tecla == 1048673):
 				print "A"
-				#drone.move_left()
+				drone.move_left()
 			#Tecla S / Move Back
 			elif (tecla == 1048691):
 				print "S"
-				#drone.move_backward()
+				drone.move_backward()
 			#Tecla D / Move Right
 			elif (tecla == 1048676):
 				print "D"
-				#drone.move_right()
+				drone.move_right()
 
 			#Tecla Up / Up
 			elif (tecla == 1113938):
 				print "Arriba"
-				#drone.move_up()
+				drone.move_up()
 			#Tecla Down / Down
 			elif (tecla == 1113940):
 				print "Abajo"
-				#drone.move_down()
+				drone.move_down()
 			#Tecla Right / Turn Right
 			elif (tecla == 1113939):
 				print "Derecha"
-				#drone.turn_right()
+				drone.turn_right()
 			#Tecla Left / Turn Left
 			elif (tecla == 1113937):
 				print "Izquierda"
-				#drone.turn_left()
+				drone.turn_left()
 			#Tecla Space / Land
 			elif (tecla == 1048608):
 				print "Espacio"
-				#drone.land()
+				drone.land()
 			#Tecla Enter / Takeoff
 			elif (tecla == 1048586):
 				print "Enter"
-				#drone.takeoff()
+				drone.takeoff()
 			#Tecla Backspace / Emergency
-			elif (tecla == ):
+			elif (tecla == 1113864):
 				print "Borrar"
-				#drone.reset()
+				drone.reset()
 			#Escape
 			elif (tecla == 1048603):
 				cv2.destroyAllWindows()
